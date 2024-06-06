26c57a6d7040f2aebc9b5c9773f661acbe8a0710
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-06 07:07:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your code here
+let arr = [];
+
+let pDiv = document.getElementById("tbl");
+
+let h3 = document.getElementById("total");
+
+let btn = document.getElementById("add");
+
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let itm = document.getElementById("item-name-input");
+    let prc = document.getElementById("item-price-input");
+
+
+    let obj = {
+        item:`${itm.value}`,
+        price:`${prc.value}`
+    }
+
+    arr.push(obj);
+
+    itm.value = ``;
+    prc.value = ``;
+
+    display(arr);
+})
+
+function display(){
+
+    let sm=0;
+
+    arr.forEach((obj)=>{
+        sm+=parseInt(obj.price);
+    })
+
+    
+    h3.textContent = `grandTotal : ${sm}`;
+    
+    let obj = arr[arr.length-1];
+
+    pDiv.innerHTML += `
+        <tr>
+            <td id="item">${obj.item}</td>
+           <td id="price">${obj.price}</td>
+        </tr>
+    `;
+}
